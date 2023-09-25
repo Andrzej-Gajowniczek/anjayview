3c931ea8dea09de85e80f8bce9f9ddc6be542c8d
gITara <andrzej.gajowniczek@gmail.com>
2023-09-25 08:48:53 +0200

    find all gfx files recursively

--- main.go
+++ main.go
@@ -3,15 +3,63 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"sort"
+	"time"
 )
 
 func main() {
-	// Get the current working directory
 	currentDir, err := os.Getwd()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	fmt.Println("Current Directory:", currentDir)
+	// List all files in the current directory
+	fileList := []string{}
+	err = filepath.Walk(currentDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && isGraphicsFile(path) {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Sort the list by creation date
+	sort.Slice(fileList, func(i, j int) bool {
+		time1, _ := getFileCreationTime(fileList[i])
+		time2, _ := getFileCreationTime(fileList[j])
+		return time1.Before(time2)
+	})
+
+	for _, file := range fileList {
+		fmt.Println(file)
+	}
+}
+
+// Function to check if a file has a graphic file extension
+func isGraphicsFile(filename string) bool {
+	extensions := []string{".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg"}
+	ext := filepath.Ext(filename)
+	for _, validExt := range extensions {
+		if ext == validExt {
+			return true
+		}
+	}
+	return false
+}
+
+// Function to get the creation time of a file
+func getFileCreationTime(filename string) (time.Time, error) {
+	fileInfo, err := os.Stat(filename)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return fileInfo.ModTime(), nil
 }
